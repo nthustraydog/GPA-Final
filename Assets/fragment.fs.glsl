a35b3847a622ab2b66bac2768d5982323e5f5001
Jack <0805jauy9131253@gmail.com>
2018-06-23 19:32:28 +0800

    MODIFY: shaders

--- Assets/fragment.fs.glsl
+++ Assets/fragment.fs.glsl
@@ -4,20 +4,22 @@ layout(location = 0) out vec4 fragColor;
 
 uniform mat4 um4mv;
 uniform mat4 um4p;
-uniform sampler2DShadow shadow_tex;
-uniform sampler2D tex;
-uniform sampler2D texture_diffuse0;
-uniform sampler2D texture_diffuse1;
 
 in VertexData
 {
-	vec4 shadow_coord;
     vec3 N; // eye space normal
     vec3 L; // eye space light vector
 	vec3 V;
     vec3 H; // eye space halfway vector
     vec2 texcoord;
+
+	vec3 FragPos;
+    vec3 TangentFragPos;
 } vertexData;
+
+uniform sampler2D tex;
+uniform sampler2D texture_diffuse0;
+uniform sampler2D texture_normal0;
 
 uniform vec3 Ia = vec3(0.15, 0.15, 0.15);
 uniform vec3 Id = vec3(1.0, 1.0, 1.0);
@@ -27,25 +29,18 @@ uniform int shinness = 8;
 
 void main()
 {
-    /*vec3 texColor = texture(tex,vertexData.texcoord).rgb;
-    fragColor = vec4(texColor, 1.0);*/
-	//Debug Only for U,V
-    //fragColor = vec4(vertexData.texcoord, 0.0, 1.0);
+	vec3 N = texture(texture_normal0, vertexData.texcoord).rgb;
 
+	N = normalize(N * 2.0 - 1.0);
 
-	vec3 N = normalize(vertexData.N); 
-	vec3 L = normalize(vertexData.L); 
-	vec3 V = normalize(vertexData.V); 
+	vec3 L = normalize(vertexData.L - vertexData.TangentFragPos);
+	vec3 V = normalize(vertexData.V - vertexData.TangentFragPos);
+	vec3 reflectDir = reflect(-L, N);
 	vec3 H = normalize(L + V);  
 	
-	float shadow_factor = textureProj(shadow_tex, vertexData.shadow_coord, 0.005);
-
-	vec3 texColor = texture(texture_diffuse0, vertexData.texcoord).rgb;
+	vec3 texColor = texture(tex, vertexData.texcoord).rgb;
 	vec3 ambient = texColor * Ia;
 	vec3 diffuse = texColor * Id * max(dot(N, L), 0.0);
 	vec3 specular = Ks * Is * pow(max(dot(N, H), 0.0), shinness);
-	fragColor = vec4(ambient, 1.0) + shadow_factor * vec4(diffuse + specular, 1.0);
-	//fragColor = vec4(shadow_factor, shadow_factor, shadow_factor, 1.0);
-	float a = texture(shadow_tex, vec3(gl_FragCoord.xy/600.0,0));
-	fragColor = vec4(a,a,a,1);
+	fragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- Assets/vertex.vs.glsl
+++ Assets/vertex.vs.glsl
@@ -3,34 +3,41 @@
 layout(location = 0) in vec3 iv3vertex;
 layout(location = 1) in vec2 iv2tex_coord;
 layout(location = 2) in vec3 iv3normal;
+layout(location = 3) in vec3 iv3tangent;
 
 uniform mat4 um4mv;
 uniform mat4 um4p;
-uniform mat4 shadow_matrix; 
+uniform mat4 um4m;
 
 out VertexData
 {
-	vec4 shadow_coord; // shadow coordinate
     vec3 N; // eye space normal
     vec3 L; // eye space light vector
 	vec3 V;
     vec3 H; // eye space halfway vector
     vec2 texcoord;
+
+	vec3 FragPos;
+    vec3 TangentFragPos;
 } vertexData;
 
 void main()
 {
-	/*gl_Position = um4p * um4mv * vec4(iv3vertex, 1.0);
-    vertexData.texcoord = iv2tex_coord;*/
-
+	mat3 normalMatrix = transpose(inverse(mat3(um4m)));
 	vec4 P = um4mv * vec4(iv3vertex, 1.0);
-	
+	vec3 T = normalize(normalMatrix * iv3tangent);
+    vec3 B = normalize(normalMatrix * cross(iv3normal, iv3tangent));
+    vec3 N = normalize(normalMatrix * iv3normal);
+	mat3 TBN = transpose(mat3(T, B, N));  
+
 	gl_Position = um4p * um4mv * vec4(iv3vertex, 1.0);
 	vertexData.texcoord = iv2tex_coord;
 
 	vertexData.N = mat3(um4mv) * iv3normal;
-	vertexData.V = -P.xyz; 
-	vertexData.L = (um4mv * vec4(0, 1, -1, 0)).xyz;
 	vertexData.H = vec3(1, 1, 1);
-	vertexData.shadow_coord = shadow_matrix * vec4(iv3vertex, 1.0);
+
+	vertexData.FragPos = vec3(um4m * vec4(iv3vertex, 1.0));   
+	vertexData.L = TBN * (um4mv * vec4(0, 1, -1, 0)).xyz;
+    vertexData.V  = TBN * (-P.xyz);
+    vertexData.TangentFragPos  = TBN * vertexData.FragPos;
 }
